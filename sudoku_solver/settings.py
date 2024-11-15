bc9116339f286ce79f5b67bb593590a3525b4974
sahilmurhekar <sahil.murhekar2004@gmail.com>
2024-11-15 11:06:50 +0530

    Modified

--- sudoku_solver/settings.py
+++ sudoku_solver/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'sudoku_solver.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default':{
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'sudokudb',
+		'HOST': 'database-1.cxggceooqrgj.ap-southeast-2.rds.amazonaws.com',
+		'USER': 'postgres',
+		'PASSWORD': 'Girendra0311',
+		'PORT': '5432',
+	}
 }
 
 
